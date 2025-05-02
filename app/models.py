22faf55a365a9127bd9ec5a08dba48f0297b8d50
João Victor - Apus <joaovictor.apus@gmail.com>
2025-05-02 17:35:45 -0300

    feat: alter database and add better names

--- app/models.py
+++ app/models.py
@@ -1,27 +1,39 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func, Index, UniqueConstraint, Time
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 from .database import Base
 
-# Definição da classe User
-class User(Base):
+# Primary key e auto incremento
+class PrimaryKey:
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+# Soft Delete e auditoria
+class AuditMixin:
+    created_at = Column(DateTime(), default=datetime.utcnow)
+    updated_at = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    deleted_at = Column(DateTime(), nullable=True)
+
+
+# User
+class User(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
     name = Column(String)
-    username = Column(String(30))
+    username = Column(String(30), unique=True, index=True)
     full_name = Column(String)
-    email = Column(String, unique=True)
-    cpf = Column(String, unique=True)
+    email = Column(String, unique=True, index=True)
+    cpf = Column(String, unique=True, index=True)
     password = Column(String)
     is_active = Column(Boolean)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
-    profile = relationship('UserProfile', back_populates='user', uselist=False)
-    roles = relationship('RoleUser', back_populates='user')
 
-class UserProfile(Base):
-    __tablename__ = 'user_profiles'
+    profile = relationship('UsersProfiles', back_populates='user', uselist=False)
+    roles = relationship('UsersRoles', back_populates='user')
+    notifications = relationship('NotificationsUsers', back_populates='user')
+    received_notifications = relationship('Notification', secondary='notifications_users', back_populates='receivers')
+
+
+class UsersProfiles(Base, AuditMixin):
+    __tablename__ = 'users_profiles'
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     registration_number = Column(String)
     father_name = Column(String(255))
@@ -30,166 +42,208 @@ class UserProfile(Base):
     work_phone = Column(String(15))
     birth_date = Column(DateTime())
     institution_id = Column(Integer, ForeignKey('institutions.id'))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    user = relationship('User', back_populates='profile')
 
-class Subject(Base):
+    user = relationship('User', back_populates='profile')
+    institution = relationship('Institution')
+
+
+class Subject(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'subjects'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, index=True)
     syllabus = Column(String)
     workload_hours = Column(Integer)
     class_duration_minutes = Column(Integer)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class Course(Base):
+    enrollments = relationship('Enrollment', back_populates='subject')
+    sessions = relationship('ClassSessions', back_populates='subject')
+
+
+class Course(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'courses'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
+    name = Column(String(255), index=True)
     duration_years = Column(Integer)
     degree = Column(String(255))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class SchoolYear(Base):
+    enrollments = relationship('Enrollment', back_populates='course')
+    sessions = relationship('ClassSessions', back_populates='course')
+
+
+class SchoolYear(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'school_years'
-    id = Column(Integer, primary_key=True)
-    year = Column(Integer)
+    year = Column(Integer, unique=True)
     start_date = Column(DateTime())
     end_date = Column(DateTime())
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class Period(Base):
+    periods = relationship('Period', back_populates='school_year')
+
+
+class Period(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'periods'
-    id = Column(Integer, primary_key=True)
     name = Column(String(255))
     school_year_id = Column(Integer, ForeignKey('school_years.id'))
     start_date = Column(DateTime())
     end_date = Column(DateTime())
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class Shift(Base):
+    school_year = relationship('SchoolYear', back_populates='periods')
+    class_groups = relationship('ClassGroup', back_populates='period')
+    sessions = relationship('ClassSessions', back_populates='period')
+
+
+class Shift(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'shifts'
-    id = Column(Integer, primary_key=True)
     name = Column(String(255))
 
-class WeekDay(Base):
+    sessions = relationship('ClassSchedule', back_populates='shift')
+
+
+class WeekDay(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'week_days'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
+    name = Column(String(255), unique=True)
 
-class Institution(Base):
+    sessions = relationship('ClassSessions', back_populates='week_day')
+
+class States(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'states'
+    name = Column(String(255), unique=True)
+    acronym = Column(String(5), unique=True)
+
+    state = relationship('Institution', back_populates='states')
+
+class Institution(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'institutions'
-    id = Column(Integer, primary_key=True)
     name = Column(String(255))
     city = Column(String(255))
-    state = Column(String(2))
+    state = Column(Integer, ForeignKey('states.id'))
     address = Column(String(255))
     is_active = Column(Boolean)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class ClassGroup(Base):
+    class_groups = relationship('ClassGroup', back_populates='institution')
+
+
+class ClassGroup(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'class_groups'
-    id = Column(Integer, primary_key=True)
     name = Column(String(255))
     period_id = Column(Integer, ForeignKey('periods.id'))
     institution_id = Column(Integer, ForeignKey('institutions.id'))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class ClassSchedule(Base):
+    period = relationship('Period', back_populates='class_groups')
+    institution = relationship('Institution', back_populates='class_groups')
+    enrollments = relationship('Enrollment', back_populates='class_group')
+    sessions = relationship('ClassSessions', back_populates='class_group')
+
+
+class ClassSchedule(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'class_schedules'
-    id = Column(Integer, primary_key=True)
-    start_time = Column(DateTime())
-    end_time = Column(DateTime())
+    start_time = Column(Time())
+    end_time = Column(Time())
+    shift_id = Column(Integer, ForeignKey('shifts.id'))
 
-class Class(Base):
-    __tablename__ = 'classes'
-    id = Column(Integer, primary_key=True)
-    teacher_id = Column(String)
+    sessions = relationship('ClassSessions', back_populates='schedule')
+
+
+class ClassSessions(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'class_sessions'
+    teacher_id = Column(Integer)
     subject_id = Column(Integer, ForeignKey('subjects.id'))
     class_group_id = Column(Integer, ForeignKey('class_groups.id'))
-    institution_id = Column(Integer, ForeignKey('institutions.id'))
-    shift_id = Column(Integer, ForeignKey('shifts.id'))
     period_id = Column(Integer, ForeignKey('periods.id'))
     course_id = Column(Integer, ForeignKey('courses.id'))
     class_schedule_id = Column(Integer, ForeignKey('class_schedules.id'))
     location = Column(String(255))
     week_day_id = Column(Integer, ForeignKey('week_days.id'))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class Absence(Base):
-    __tablename__ = 'absences'
-    id = Column(Integer, primary_key=True)
-    class_id = Column(Integer, ForeignKey('classes.id'))
+    subject = relationship('Subject', back_populates='sessions')
+    class_group = relationship('ClassGroup', back_populates='sessions')
+    shift = relationship('Shift', back_populates='sessions')
+    period = relationship('Period', back_populates='sessions')
+    course = relationship('Course', back_populates='sessions')
+    schedule = relationship('ClassSchedule', back_populates='sessions')
+    week_day = relationship('WeekDay', back_populates='sessions')
+    absences = relationship('StudentAbsences', back_populates='class_session')
+
+
+class StudentAbsences(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'student_absences'
+    class_id = Column(Integer, ForeignKey('class_sessions.id'))
     enrollment_id = Column(Integer, ForeignKey('enrollments.id'))
     justify = Column(Boolean)
     justify_types = Column(Integer)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class EnrollmentStatus(Base):
-    __tablename__ = 'enrollment_statuses'
-    id = Column(Integer, primary_key=True)
-    status = Column(String(255))
+    enrollment = relationship('Enrollment', back_populates='absences')
+    class_session = relationship('ClassSessions', back_populates='absences')
 
-class Enrollment(Base):
+
+class EnrollmentsStatus(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'enrollments_status'
+    status = Column(String(255), unique=True)
+
+    enrollments = relationship('Enrollment', back_populates='status')
+
+
+class Enrollment(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'enrollments'
-    id = Column(Integer, primary_key=True)
-    student_id = Column(String)
+    student_id = Column(Integer, ForeignKey('users.id'))
     subject_id = Column(Integer, ForeignKey('subjects.id'))
     course_id = Column(Integer, ForeignKey('courses.id'))
     class_group_id = Column(Integer, ForeignKey('class_groups.id'))
-    status_id = Column(Integer, ForeignKey('enrollment_statuses.id'))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
+    status_id = Column(Integer, ForeignKey('enrollments_status.id'))
 
-class Role(Base):
+    student = relationship('User')
+    subject = relationship('Subject', back_populates='enrollments')
+    course = relationship('Course', back_populates='enrollments')
+    class_group = relationship('ClassGroup', back_populates='enrollments')
+    status = relationship('EnrollmentsStatus', back_populates='enrollments')
+    absences = relationship('StudentAbsences', back_populates='enrollment')
+
+
+class Role(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'roles'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
+    name = Column(String(255), nullable=False, unique=True)
 
-class RoleUser(Base):
-    __tablename__ = 'role_user'
+    permissions = relationship('RolePermission', back_populates='role')
+    users = relationship('UsersRoles', back_populates='role')
+
+
+class UsersRoles(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'users_roles'
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     role_id = Column(Integer, ForeignKey('roles.id'), primary_key=True)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
+
     user = relationship('User', back_populates='roles')
+    role = relationship('Role', back_populates='users')
 
-class Permission(Base):
+
+class Permission(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'permissions'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    permission = Column(String(255))
+    permission = Column(String(255), unique=True)
     description = Column(String)
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
 
-class RolePermission(Base):
+    roles = relationship('RolePermission', back_populates='permission')
+
+
+class RolePermission(PrimaryKey, Base, AuditMixin):
     __tablename__ = 'role_permissions'
-    id = Column(Integer, primary_key=True, autoincrement=True)
     role_id = Column(Integer, ForeignKey('roles.id'))
     permission_id = Column(Integer, ForeignKey('permissions.id'))
-    created_at = Column(DateTime())
-    updated_at = Column(DateTime())
-    deleted_at = Column(DateTime())
+
+    role = relationship('Role', back_populates='permissions')
+    permission = relationship('Permission', back_populates='roles')
+
+
+class Notification(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'notifications'
+    title = Column(String(50))
+    text = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User")
+    receivers = relationship("User", secondary="notifications_users", back_populates="received_notifications")
+
+
+class NotificationsUsers(PrimaryKey, Base, AuditMixin):
+    __tablename__ = 'notifications_users'
+    notifications_id = Column(Integer, ForeignKey('notifications.id'))
+    users_id = Column(Integer, ForeignKey('users.id'))
+
+    notification = relationship('Notification')
+    user = relationship('User', back_populates='notifications')
